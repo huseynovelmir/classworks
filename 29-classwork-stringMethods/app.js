5b764303a7df4ca8e2a4fdd53eea2bddb269721c
sabinabakirzada <huseynovelmir789@gmail.com>
2024-04-05 12:40:52 +0400

    done

---
+++ 29-classwork-stringMethods/app.js
@@ -0,0 +1,330 @@
+let students = [
+  {
+    id: 1,
+    name: "Nurane",
+    surname: "Ismayilzade",
+    age: 21,
+    hobbies: ["music", "walking", "watchingfilm"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "cavid" },
+      { id: 4, name: "alisa" },
+    ],
+    loginDetail: { username: "nurana21", password: "nunu123" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 144,
+  },
+  {
+    id: 2,
+    name: "Gizilgul",
+    surname: "Allahverdiyeva",
+    age: 20,
+    hobbies: ["book", "writing code"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "babaxan" },
+      { id: 4, name: "gulshen" },
+    ],
+    loginDetail: { username: "allahverdieva1", password: "salam123" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 70,
+    salaryAZN: 100,
+  },
+  {
+    id: 3,
+    name: "Xanim",
+    surname: "Nuriyeva",
+    age: 21,
+    hobbies: ["book", "music"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "hikmet" },
+      { id: 4, name: "gulsen" },
+    ],
+    loginDetail: { username: "xanim123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 145,
+  },
+  {
+    id: 4,
+    name: "Minaya",
+    surname: "Binnetova",
+    age: 21,
+    hobbies: ["book", "gaming", "movie", "music"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "anar" },
+    ],
+    loginDetail: { username: "binnetova", password: "hello123" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 90,
+    salaryAZN: 142,
+  },
+  {
+    id: 5,
+    name: "Sabina",
+    surname: "Hatamli",
+    age: 21,
+    hobbies: ["shopping", "listen to music"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "Mirvari" },
+    ],
+    loginDetail: { username: "sebine123", password: "salam123" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 81,
+    salaryAZN: 146,
+  },
+
+  {
+    id: 6,
+    name: "Ləman",
+    surname: "Şamilova",
+    age: 20,
+    hobbies: [
+      "watching movies",
+      "reading books",
+      "painting",
+      " walking",
+      " listen to music",
+    ],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+    ],
+    loginDetail: { username: "lemanshamilova", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 85.8,
+    salaryAZN: 145,
+  },
+  {
+    id: 7,
+    name: "Narmin",
+    surname: "Musayeva",
+    age: 21,
+    hobbies: ["book", "gaming", "painting", "walking"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "fidan" },
+      { id: 4, name: "Irada" },
+    ],
+    loginDetail: {
+      username: "narmin0",
+      password: "narmin01",
+    },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 196,
+  },
+
+  {
+    id: 8,
+    name: "Fatima",
+    surname: "Akhundzada",
+    age: 20,
+    hobbies: ["drawing", "sleeping"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "Valida" },
+    ],
+    loginDetail: { username: "aynur123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 76,
+    salaryAZN: 100,
+  },
+  {
+    id: 9,
+    name: "Elmir",
+    surname: "Huseynov",
+    age: 21,
+    hobbies: ["book", "ice skating", "Tennis", "Karting"],
+    student: true,
+    teacher: [
+      { id: 1, name: "Gurban" },
+      { id: 2, name: "Hajar" },
+      { id: 3, name: "Morad" },
+      { id: 4, name: "Fikrat" },
+    ],
+    loginDetail: { username: "huseynovelmirr", password: "maxverstappen" },
+    gender: "man",
+    boyfriendGirlfriend: false,
+    fail: true,
+    avgPoint: 76,
+    salaryAZN: 120,
+  },
+  {
+    id: 10,
+    name: "Fidan",
+    surname: "Rehimli",
+    age: 21,
+    hobbies: ["book", "gaming"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "Turan" },
+    ],
+    loginDetail: { username: "fidan123", password: "fidanrahimli" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: true,
+    avgPoint: 75,
+    salaryAZN: 98,
+  },
+  {
+    id: 11,
+    name: "Aynur",
+    surname: "Aynurova",
+    age: 20,
+    hobbies: ["book", "gaming"],
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+    ],
+    loginDetail: { username: "aynur123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: true,
+    avgPoint: 81,
+    salaryAZN: 120,
+  },
+
+  {
+    id: 12,
+    name: "Elman",
+    surname: "Muradov",
+    age: 20,
+    hobbies: [
+      "book",
+      "gaming",
+      "sking",
+      "wrestling",
+      "swiming",
+      "arguing about philosophy",
+      "solving sudoku",
+      "hiking",
+      "walking",
+    ],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "jale" },
+      { id: 4, name: "akhmed" },
+    ],
+    loginDetail: { username: "elman17", password: "elman12345" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: true,
+    avgPoint: 76,
+    salaryAZN: 100,
+  },
+];
+
+//! Tələbələrin təqaüdünü manat və dollarla göstər
+// for (let i = 0; i < students.length; i++) {
+//   console.log(
+//     students[i].salaryAZN,
+//     "Manat",
+//     (students[i].salaryAZN / 1.7).toFixed(1),
+//     "$"
+//   );
+// }
+//! - Müəllimlərin baş hərflərini böyük hərflə yazın
+// for (let i = 0; i < students.length; i++) {
+//   for (let j = 0; j < students[i].teacher.length; j++) {
+//     console.log(
+//       students[i].teacher[j].name.charAt(0).toUpperCase() +
+//         students[i].teacher[j].name.slice(1)
+//     );
+//   }
+// }
+//! - Şifrəsində rəqəm olmayan tələbələri tapın
+// let arr = [];
+// for (let i = 0; i < students.length; i++) {
+//   let obj = {};
+//   //   console.log(students[i].loginDetail.password);
+//   let pass = students[i].loginDetail.password;
+
+//   for (let j = 0; j < pass.length; j++) {
+//     // const element = array[j];
+//     // console.log(pass[j], +pass[j]);
+//     if (pass[j] == +pass[j]) {
+//       //   console.log(pass[j]);
+//       //   console.log(students[i].name);
+//       obj.name = students[i].name;
+
+//       obj.password = pass;
+//       //   console.log(obj);
+//       arr.push(obj);
+//       break;
+//     }
+//   }
+// }
+// console.log(arr);
+//! - Qızların arasında id-si 3 olan müəllimlərin adını deyin
+
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].gender == "girl") {
+//     for (let j = 0; j < students[i].teacher.length; j++) {
+//       if (students[i].teacher[j].id == 3) {
+//         console.log(
+//           "Tələbə:",
+//           students[i].name,
+//           "Müəllim:",
+//           students[i].teacher[j].name
+//         );
+//       }
+//     }
+//   }
+// }
+//! - Tələbələrin adlarını və müəllim saylarını çapa verin
+// for (let i = 0; i < students.length; i++) {
+//   console.log(
+//     "Tələbə:",
+//     students[i].name,
+//     "Müəllim sayı:",
+//     students[i].teacher.length
+//   );
+// }
+//! - Tələbələrin adlarını username və şifrələrindən ibarət yeni array yaradın
+
+// let newArr = students.map(({ id, name, loginDetail }) => ({
+//   id,
+//   name,
+//   loginDetail,
+// }));
+// console.log(newArr);
