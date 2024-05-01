1f0d38bd486aa0a1dfb7e96531b57c83328f521b
huseynovelmir <huseynovelmir789@gmail.com>
2024-05-01 11:01:15 +0400

    done

---
+++ 40-hookForm/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormPropsTextFields from "./components/FormPropsTextFields";
+
+function App() {
+  return (
+    <>
+      <FormPropsTextFields />
+    </>
+  );
+}
+
+export default App;
